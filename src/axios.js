398c26d535681fe1235e90ec46c0ea9d46685931
SakethKumarTallam <sakethkumar594@gmail.com>
2025-07-11 13:15:45 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const instance = axios.create({
     // baseURL: "http://localhost:5001/api"  // Ensure this matches your backend port
     // baseURL: "https://crbackend-lixw.onrender.com/api"  // Ensure this matches your backend port
-    baseURL: "https://crbackend-9hhi.onrender.com/api"  // Ensure this matches your backend port
+    // baseURL: "https://crbackend-9hhi.onrender.com/api"  // Ensure this matches your backend port
+    baseURL: "https://carrentalbackend-3aci.onrender.com/api"  // Ensure this matches your backend port
 });
 
 export default instance;
